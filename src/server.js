4acf44cdf724b3d450b9ea1b08c8b2a1ff44040e
Caca Wilda Andika <cacawildaandika@gmail.com>
2021-08-24 21:19:47 +0700

    update return value in endpoint info

--- src/server.js
+++ src/server.js
@@ -21,7 +21,7 @@ const init = async () => {
             method: 'GET',
             path: '/info',
             handler: () => {
-                return 'Endpoint info';
+                return 'Info page';
             }
         },
     ])
